096ff9bdb744f9a2b36c8d1a4412351f64cdfe93
Flavia <153841117+flaviacmc@users.noreply.github.com>
2024-01-26 15:57:03 -0300

    Update litispendenciaNewDossie.ts - alteração de logica de litispendencia para o dossiê Sapiens 2

--- src/modules/GetInformationFromSapienForSamir/DossieSuperSapiens/SuperDossieBusiness/GetInformationLitispedencia/litispendenciaNewDossie.ts
+++ src/modules/GetInformationFromSapienForSamir/DossieSuperSapiens/SuperDossieBusiness/GetInformationLitispedencia/litispendenciaNewDossie.ts
@@ -3,13 +3,24 @@ import { getXPathText } from "../../../../../helps/GetTextoPorXPATH";
                                                
 export class LitispedenciaNewDossie{
     async funcLitis(parginaDosPrevFormatada: any){
+        const xpathProcessoJudicial = "/html/body/div/div[5]/table/tbody/tr/td[1]";
+        const xpathCNJ = "/html/body/div/div[4]/table/tbody/tr[1]/td";                    
+        const xpathProcessoJudicialFormatado: string = (getXPathText(parginaDosPrevFormatada, xpathProcessoJudicial).trim().replace(/[-.]/g, ""));
+        const xpathCNJFormatado: string = (getXPathText(parginaDosPrevFormatada, xpathCNJ).trim().replace(/[-.]/g, ""));
+        const xpathRelacaoProcessoMovidosFormatada:boolean = xpathProcessoJudicialFormatado===xpathCNJFormatado;
+        //console.log("CNJ: ", xpathCNJFormatado)
+        //console.log("Processo Judicial: ", xpathProcessoJudicialFormatado)
+        //console.log("Litispendência boolean: ",xpathRelacaoProcessoMovidosFormatada);
+        return xpathRelacaoProcessoMovidosFormatada;
+    }
+    /*async funcLitis(parginaDosPrevFormatada: any){
         const xpathRelacaoProcesso = "/html/body/div/div[5]/table/tbody/tr";                   
         const xpathRelacaoProcessoFormatada: string = (getXPathText(parginaDosPrevFormatada, xpathRelacaoProcesso).trim());
         const StringParaVerificar: string = "Não há relação dos processos movidos pelo autor contra o INSS.";
         const xpathRelacaoProcessoMovidosFormatada:boolean = xpathRelacaoProcessoFormatada===StringParaVerificar;
         return xpathRelacaoProcessoMovidosFormatada;
         
-    }
+    }*/
     
 }
 
