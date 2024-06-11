51bc8c2bb30d00e1ef2347e50db55ddc930dfebb
Luan Henrique <luanhsantos14@gmail.com>
2024-06-11 16:41:44 -0300

    feat: criar função para armazenar envolvidos
    envolvidos já cadastrados

---
+++ src/modules/CreateInterested/Helps/ArrayInteressados.ts
@@ -0,0 +1,41 @@
+interface Record {
+    pessoa: {
+        numeroDocumentoPrincipal: string;
+    }
+}
+
+interface Result {
+    records: Record[];
+}
+
+interface InputObject {
+    result: Result;
+}
+
+type InputArray = InputObject[];
+
+
+export function arrayInteressados(inputArray: InputArray): string[] {
+
+    if (inputArray.length === 0 || typeof inputArray[0] !== 'object') {
+        throw new Error("O array de entrada deve conter um único objeto.");
+    }
+
+    const objeto = inputArray[0];
+    const resultado: string[] = [];
+
+    if (objeto.result && Array.isArray(objeto.result.records)) {
+        for (let i = 0; i < objeto.result.records.length; i++) {
+            if (objeto.result.records[i].pessoa.numeroDocumentoPrincipal) {
+                resultado.push(objeto.result.records[i].pessoa.numeroDocumentoPrincipal);
+            }
+        }
+    } else {
+        throw new Error("O caminho objeto.result.records não existe ou não é um array.");
+    }
+
+    console.log(resultado)
+
+    return resultado;
+    
+}
