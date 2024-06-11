3f0cdb244417904b242b16b3fe8d3125f3cc8578
Luan Henrique <luanhsantos14@gmail.com>
2024-06-11 16:39:43 -0300

    feat: etiquetagem dos envolvidos

--- src/modules/CreateInterested/CreateInterestedUseCase.ts
+++ src/modules/CreateInterested/CreateInterestedUseCase.ts
@@ -5,6 +5,7 @@ import { getUsuarioUseCase } from "../GetUsuario";
 import { getTarefaUseCase } from "../GetTarefa";
 import { GetPessoa_id } from "./RequisicaoAxiosTarefas/GetPessoa_id";
 import { CreateTarefa } from "./RequisicaoAxiosTarefas/CreateTarefa";
+import { GetInteressadosReq } from "./RequisicaoAxiosTarefas/GetInteressadosReq";
 import { IGetArvoreDocumentoDTO } from "../../DTO/GetArvoreDocumentoDTO";
 import { getArvoreDocumentoUseCase } from "../GetArvoreDocumento";
 import { getDocumentoUseCase } from "../GetDocumento";
@@ -16,6 +17,8 @@ import { getXPathText } from "../../helps/GetTextoPorXPATH";
 import { buscarTableCpf } from "../GetInformationFromSapienForSamir/helps/procurarTableCpf";
 import { updateEtiquetaUseCase } from "../UpdateEtiqueta";
 import { CorrigirCpfComZeros } from "./Helps/CorrigirCpfComZeros";
+import { arrayInteressados } from "./Helps/ArrayInteressados";
+import { error } from "console";
 
 export class CreateInterestedUseCase{
 
@@ -30,10 +33,13 @@ export class CreateInterestedUseCase{
 
         let tarefas = await getTarefaUseCase.execute({ cookie, usuario_id, etiqueta: data.etiqueta });
         let arrayDeDocumentos: ResponseArvoreDeDocumento[];
+
+        
         
         for(let i = 0; i < tarefas.length; i++){
 
             try{
+                
 
                 const tarefaId = tarefas[i].id
                 const objectGetArvoreDocumento: IGetArvoreDocumentoDTO = { nup: tarefas[i].pasta.NUP, chave: tarefas[i].pasta.chaveAcesso, cookie, tarefa_id: tarefas[i].id }
@@ -97,22 +103,45 @@ export class CreateInterestedUseCase{
                     continue;
                 }
     
-               
+                let InputArray = await GetInteressadosReq(tarefas[i].pasta_id, cookie)
+                console.log('--RESPOSTA:')
+                console.log(InputArray)
+                let ArrayEnvolvidos = arrayInteressados(InputArray)
                 
                 
-                 const buscarTabelaGrupoFamiliar = new BuscarTabelaGrupoFamiliar;
-                 const arrayCpfsInteressados = await buscarTabelaGrupoFamiliar.execute(paginaDossieSocialFormatada);  
+                const buscarTabelaGrupoFamiliar = new BuscarTabelaGrupoFamiliar;
+                const arrayCpfsInteressados = await buscarTabelaGrupoFamiliar.execute(paginaDossieSocialFormatada); 
+                 
+                
+                try {
+                    for(let j = 0; j < arrayCpfsInteressados.length; j++ ){
+                        console.log('aquiiiiiiiiiiii ' + CorrigirCpfComZeros(arrayCpfsInteressados[j].trim()))
+                        let cpfExistente = ArrayEnvolvidos.filter((element) => element === CorrigirCpfComZeros(arrayCpfsInteressados[j].trim()))
+                        console.log(cpfExistente)
+    
+                        if(CorrigirCpfComZeros(arrayCpfsInteressados[j].trim()) !== cpfCapa.trim() && !cpfExistente.length){
+                            const pessoa_id =  await GetPessoa_id(CorrigirCpfComZeros(arrayCpfsInteressados[j].trim()), cookie)
+                            if (!pessoa_id) {
+                                throw new Error('CPF NÃO CONSTA NA RECEITA')
+                            } else {
+                                const pasta_id = tarefas[i].pasta_id;
+                                await CreateTarefa(pasta_id, pessoa_id, cookie)
+                            }
+                        }
+                    }
+
+                    await updateEtiquetaUseCase.execute({ cookie, etiqueta: `ENVOLVIDOS CADASTRADOS`, tarefaId })
+
+                } catch (error) {
+                    if (error.message === 'CPF NÃO CONSTA NA RECEITA') {
+                        await updateEtiquetaUseCase.execute({ cookie, etiqueta: `CPF ENVOLVIDO NÃO CONSTA NA RECEITA`, tarefaId })
+                    } else {
+                        await updateEtiquetaUseCase.execute({ cookie, etiqueta: `ERRO AO CADASTRAR ENVOLVIDOS`, tarefaId })
+                    }
+
+                }
 
     
-                for(let j = 0; j < arrayCpfsInteressados.length; j++ ){
-                    console.log('aquiiiiiiiiiiii ' + CorrigirCpfComZeros(arrayCpfsInteressados[j].trim()))
-                    if(CorrigirCpfComZeros(arrayCpfsInteressados[j].trim()) !== cpfCapa.trim()){
-                        const pessoa_id =  await GetPessoa_id(CorrigirCpfComZeros(arrayCpfsInteressados[j].trim()), cookie)
-                        const pasta_id = tarefas[i].pasta_id;
-                         await CreateTarefa(pasta_id, pessoa_id, cookie)
-                    }
-                }
-                await updateEtiquetaUseCase.execute({ cookie, etiqueta: `ENVOLVIDOS CADASTRADOS`, tarefaId })
             }catch(e){
                 console.log(e)
                 return e
