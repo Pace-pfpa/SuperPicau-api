67c553235b97695f195b1999a30e6f14630d5818
Luan Henrique <luanhsantos14@gmail.com>
2024-12-02 16:27:02 -0300

    refactor[UpdateDocument]

--- src/modules/UploadDocument/UploadDocumentController.ts
+++ src/modules/UploadDocument/UploadDocumentController.ts
@@ -2,11 +2,11 @@ import { Request, Response } from 'express';
 import { UploadDocumentUseCase } from "./UploadDocumentUseCase";
 
 export class UploadDocumentController {
-    constructor(private UploadDocumentUseCase: UploadDocumentUseCase,) { }
+    constructor(private readonly UploadDocumentUseCase: UploadDocumentUseCase,) { }
     async handle(request: Request, response: Response): Promise<Response> {
-        const { cookie, fileName, conteudo, tipo_documento, documento_id } = request.body;
+        const { cookie, fileName, conteudo, documento_id } = request.body;
         try {
-            const response = await this.UploadDocumentUseCase.execute(cookie, fileName, conteudo, documento_id, tipo_documento);
+            const response = await this.UploadDocumentUseCase.execute(cookie, fileName, conteudo, documento_id);
             response.status(200).json(response);
         } catch (error) {
             return response.status(400).json({
