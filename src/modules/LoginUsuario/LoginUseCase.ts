58255f4c4897844b2aee20ef08049085da6e4567
Luan Henrique <luanhsantos14@gmail.com>
2024-12-02 16:26:07 -0300

    refactor[LoginUsuario]

--- src/modules/LoginUsuario/LoginUseCase.ts
+++ src/modules/LoginUsuario/LoginUseCase.ts
@@ -1,13 +1,12 @@
-import { ILoginDTO } from '../../DTO/LoginDTO'
 import { LoginSapiens } from '../../pytonRequest/loginSapiens'
+import { LoginDTO } from './dtos/LoginDTO';
 
 export class LoginUseCase {
-    async execute(data: ILoginDTO): Promise<string> {
+    async execute(data: LoginDTO): Promise<string> {
         console.log("login inicializado")
         const loginIsTrue = await LoginSapiens(data)
         const verifyBoolean = loginIsTrue.split("(")[1].split(",")[0].trim();
         if(verifyBoolean == "False") throw new Error()
-        return await loginIsTrue.split("(")[1].split("'")[1];
-        //return await LoginSapiens(data);
+        return loginIsTrue.split("(")[1].split("'")[1];
     }
 }
