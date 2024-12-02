601c26f9b3f0d48058ad0333173c6ed24d086c36
Luan Henrique <luanhsantos14@gmail.com>
2024-12-02 16:25:17 -0300

    refactor[GetTarefaNup]

--- src/modules/GetTarefaNup/GetTarefaNupController.ts
+++ src/modules/GetTarefaNup/GetTarefaNupController.ts
@@ -2,12 +2,12 @@ import { Request, Response } from 'express';
 import { GetTarefaNupUseCase } from './GetTarefaNupUseCase';
 
 export class GetTarefaNupController {
-    constructor(private GetUsuarioNupUseCase: GetTarefaNupUseCase,) { }
+    constructor(private readonly getUsuarioNupUseCase: GetTarefaNupUseCase,) { }
     async handle(request: Request, response: Response): Promise<Response> {
         const { cookie, nup } = request.body;
         const {usuario_id} = request.params;
         try {
-            const result = await this.GetUsuarioNupUseCase.execute({cookie, usuario_id, nup});
+            const result = await this.getUsuarioNupUseCase.execute({cookie, usuario_id, nup});
             response.status(200).json(result);
         } catch (error) {
             return response.status(400).json({
