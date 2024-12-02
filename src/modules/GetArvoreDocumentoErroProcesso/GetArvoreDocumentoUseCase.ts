c054fd3ea15c1472d1617d64d2c7641df946c89f
Luan Henrique <luanhsantos14@gmail.com>
2024-12-02 16:17:53 -0300

    refactor[GetArvoreDocumento]

--- src/modules/GetArvoreDocumentoErroProcesso/GetArvoreDocumentoUseCase.ts
+++ src/modules/GetArvoreDocumentoErroProcesso/GetArvoreDocumentoUseCase.ts
@@ -1,27 +1,17 @@
 import axios from "axios";
-import { IGetArvoreDocumentoDTO } from "../../DTO/GetArvoreDocumentoDTO";
 import { RequestHeaders } from "../../sapiensOperations/resquest/RequestHeaders";
-import { ResponseArvoreDeDocumento } from "../../sapiensOperations/response/ResponseArvoreDeDocumento";
-
-
+import { GetArvoreDocumentoDTO, ResponseArvoreDeDocumentoDTO } from "../GetArvoreDocumento";
 
 export class ErroGetArvoreDocumentoUseCase {
 
-    async execute(data: IGetArvoreDocumentoDTO): Promise<Array<ResponseArvoreDeDocumento>> {
-        const now = Date.now();
+    async execute(data: GetArvoreDocumentoDTO): Promise<Array<ResponseArvoreDeDocumentoDTO>> {
         const requestHeaderUploadArquivo = new RequestHeaders;
         const headers = await requestHeaderUploadArquivo.execute(data.cookie);
         const baseURL = `https://sapiens.agu.gov.br/visualizador?nup=${data.nup}`
-        //console.log("URL"+ baseURL)
-        // console.log(headers);
         return await axios.get(baseURL, {headers}).then(response =>{
-            //console.log(response.status)
-            //console.log(response.data)
             return response.data;
         }).catch(error =>{
-            //console.log(error)
             return new Error(error);
         })
-        // return await UploadDocumenteForSapienInPython(Coockie, fileName, conteudo, documento_id, tipo_documento);
     }
 }
