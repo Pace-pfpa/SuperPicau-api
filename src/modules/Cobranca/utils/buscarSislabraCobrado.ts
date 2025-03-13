0e4c42bdb9a81c46a2fae7209f86efe9eab985f5
Luan Henrique <luanhsantos14@gmail.com>
2025-03-13 14:31:04 -0300

    feat(cobranca): improve minuta cobranca

--- src/modules/Cobranca/utils/buscarSislabraCobrado.ts
+++ src/modules/Cobranca/utils/buscarSislabraCobrado.ts
@@ -5,36 +5,38 @@ import { getDocumentoUseCase } from "../../GetDocumento";
 import { verificarGeracaoComponentes } from "../../GetInformationFromSapiensForPicaPau/utils/verificarGeracaoComponentes";
 
 /**
- * Função auxiliar para buscar o sislabra do requerido do processo, para processos de cobrança.
- * A função filtra o sislabra do requerido (PÓLO PASSIVO), a partir de um array de documentos SAPIENS, e o formata.
+ * Função auxiliar para buscar os sislabras dos requeridos do processo, para processos de cobrança.
+ * A função filtra os sislabras dos requeridos (PÓLO PASSIVO), a partir de um array de documentos SAPIENS, e os formata.
  * @param arrayDeDocumentos Array de documentos SAPIENS.
  * @param cookie Cookie de usuário para interações com o SAPIENS.
- * @returns O sislabra do requerido (JSDOM) ou null.
+ * @returns Os sislabra do requerido (JSDOM) ou array vazio.
  */
 export async function buscarSislabraCobrado(
     arrayDeDocumentos: ResponseArvoreDeDocumentoDTO[],
     cookie: string
-): Promise<{ sislabraCobrado: JSDOMType }> {
-    let sislabraCobrado: ResponseArvoreDeDocumentoDTO = null;
-    let sislabraCobradoFormatado: JSDOMType = null;
+): Promise<{ sislabraCobrado: JSDOMType[] }> {
+    let sislabraCobrado: ResponseArvoreDeDocumentoDTO[] = [];
+    let sislabraCobradoFormatado: JSDOMType[] = [];
 
     const documentosRequerido = arrayDeDocumentos.filter((doc) => doc.movimento?.indexOf("PÓLO PASSIVO") !== -1);
     
     for (const documento of documentosRequerido) {
         const isErroComponente = await verificarGeracaoComponentes(documento, cookie)
         if (isErroComponente instanceof Error) continue;
-        sislabraCobrado = documento;
+        sislabraCobrado.push(documento);
     }
 
-    try {
-        const idSislabraCobrado = sislabraCobrado.documentoJuntado.componentesDigitais[0].id;
-        const paginaSislabraCobrado = await getDocumentoUseCase.execute({ cookie, idDocument: idSislabraCobrado });
-        const paginaFormatadaCobrado = new JSDOM(paginaSislabraCobrado);
-        
-        sislabraCobradoFormatado = paginaFormatadaCobrado;
-    } catch (error) {
-        console.error(error.message);
-        return { sislabraCobrado: null }
+    for (const sislabra of sislabraCobrado) {
+        try {
+            const idSislabraCobrado = sislabra.documentoJuntado.componentesDigitais[0].id;
+            const paginaSislabraCobrado = await getDocumentoUseCase.execute({ cookie, idDocument: idSislabraCobrado });
+            const paginaFormatadaCobrado = new JSDOM(paginaSislabraCobrado);
+            
+            sislabraCobradoFormatado.push(paginaFormatadaCobrado);
+        } catch (error) {
+            console.error(error.message);
+            continue
+        }
     }
 
     return { sislabraCobrado: sislabraCobradoFormatado }
--- src/modules/Cobranca/utils/renderSecaoCobranca.ts
+++ src/modules/Cobranca/utils/renderSecaoCobranca.ts
@@ -1,19 +1,29 @@
 import { renderTable } from "../../CreateHtmlForRM/utils";
+import { CobrancaLabras } from "../types/CobrancaLabras.types";
 
 export const renderSecaoCobranca = (
     encontrado: boolean,
     titulo: string,
-    dados: any[] | undefined,
-    colunas: string[],
-    tituloCobrado: string,
+    dados: CobrancaLabras[],
+    colunas: string[]
 ): string => {
     if (!encontrado) return "";
+
+    const empresasEncontradas = dados
+        .map((membro, _index) =>
+          membro.impeditivos.empresas.length
+            ? renderTable(
+                membro.impeditivos.empresas,
+                'Empresas da Parte' + ` - ${membro.nome}`,
+                colunas
+              ) : ''
+        )
+        .join("");
 
     return `
         <p>
             <strong><span>${titulo}</span></strong>:
-            <p><br><p>
         </p>
-        ${renderTable(dados, tituloCobrado, colunas)}
+        ${empresasEncontradas}
     `;
 }
